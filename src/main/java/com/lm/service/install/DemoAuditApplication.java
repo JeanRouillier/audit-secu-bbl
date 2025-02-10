1d1adb6913d0b53a2baf176bc3bafc52d6bbc40e
JEAN <jean.rouillier@gmail.com>
2025-02-10 17:29:22 +0100

    cleaning

--- src/main/java/com/lm/service/install/DemoAuditApplication.java
+++ src/main/java/com/lm/service/install/DemoAuditApplication.java
@@ -2,8 +2,12 @@ package com.lm.service.install;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+@EnableWebSecurity
 public class DemoAuditApplication {
 
 	public static void main(String[] args) {
